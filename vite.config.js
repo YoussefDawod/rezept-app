c0dddc14c1a1374f3d09773d2a35f0f8821eec9c
youssef-dawod <youssef-dawod>
2024-11-13 14:04:31 +0100

    Update vite.config.js with base URL

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rezept-app/', // Setzen Sie dies auf den Namen Ihres Repositories
   plugins: [react()],
 })
